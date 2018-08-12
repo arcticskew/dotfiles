fd281bd3ac1ee6289e5ab84d982c07df14bd99ce
Austin Cross <austin@skew-t.com>
2018-08-11 20:42:13 -0500

    sync force option

--- sync.py
+++ sync.py
@@ -5,6 +5,7 @@ Based on https://gist.github.com/490016
 """
 
 import os
+import sys
 import glob
 import shutil
 
@@ -24,16 +25,17 @@ def is_link_to(link, dest):
     is_link = is_link and os.readlink(link).rstrip('/') == dest.rstrip('/')
     return is_link
 
-def link(src, dst):
+def link(src, dst, force=False):
     # Check that we aren't overwriting anything
     if os.path.lexists(dst):
         if is_link_to(dst, src):
             return
 
-        response = raw_input("Overwrite file `%s'? [y/N] " % dst)
-        if not response.lower().startswith('y'):
-            print "Skipping `%s'..." % dst
-            return
+        if not force:
+            response = raw_input("Overwrite file `%s'? [y/N] " % dst)
+            if not response.lower().startswith('y'):
+                print "Skipping `%s'..." % dst
+                return
 
         force_remove(dst)
 
@@ -41,6 +43,12 @@ def link(src, dst):
     print "%s => %s" % (dst, src)
 
 def main():
+    force = False
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-f':
+            force = True
+
     os.chdir(os.path.expanduser(SOURCE_DIR))
     for filename in [file for file in glob.glob('*') if file not in EXCLUDE]:
         dotfile = filename
@@ -51,7 +59,7 @@ def main():
         dotfile = os.path.join(os.path.expanduser('~'), dotfile)
         source = os.path.join(SOURCE_DIR, filename).replace('~', '.')
 
-        link(source, dotfile)
+        link(source, dotfile, force)
 
 if __name__ == '__main__':
     main()
